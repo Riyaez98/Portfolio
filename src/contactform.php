eab4f093aced7b70ba2c25e938317ba9d715d41a
Ricardo Yanez <riyaez@gmail.com>
2021-10-13 19:04:34 -0400

    form again

--- src/contactform.php
+++ src/contactform.php
@@ -1,15 +1,15 @@
-<?php 
+//<?php 
+//
+//if(isset($_POST['submit'])){
+//
+//    $name = $_POST['name'];
+//    $mailFrom = $_POST['email'];
+//    $message = $_POST['message'];
+//
+//    $mailTo = "contact@ryanez.ca";
+//    $headers = "From: ".$mailFrom;
+//    $txt = "Vous avez reçu un courriel de la part de".$name.".\n\n".$message;
 
-if(isset($_POST['submit'])){
-
-    $name = $_POST['name'];
-    $mailFrom = $_POST['email'];
-    $message = $_POST['message'];
-
-    $mailTo = "contact@ryanez.ca";
-    $headers = "From: ".$mailFrom;
-    $txt = "Vous avez reçu un courriel de la part de".$name.".\n\n".$message;
-
-    mail($mailTo, $name, $txt, $headers);
-    header("Location: contact.html?mailsend");
-}
+//    mail($mailTo, $name, $txt, $headers);
+//    header("Location: contact.html?mailsend");
+//}
