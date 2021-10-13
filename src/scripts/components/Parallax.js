7aef54275c0c3940241ab9998b31af7cf689ff7c
Ricardo Yanez <riyaez@gmail.com>
2021-10-13 13:46:51 -0400

    petit ajustement parallax et hero accueil

--- src/scripts/components/Parallax.js
+++ src/scripts/components/Parallax.js
@@ -18,8 +18,8 @@ export default class Parallax {
       this.querySelectorAll('.layer').forEach((layer) => {
         const speed = layer.getAttribute('data-speed');
 
-        const x = (window.innerWidth - e.pageX * speed) / 150;
-        const y = (window.innerHeight - e.pageY * speed) / 150;
+        const x = (window.innerWidth - e.pageX * speed) / 100;
+        const y = (window.innerHeight - e.pageY * speed) / 100;
         layer.style.transform = `translateX(${x}px) translateY(${y}px)`;
       });
     }
