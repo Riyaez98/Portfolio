edf188d934354ae7ce8168229d4794a6c8fd7f1b
Ricardo Yanez <riyaez@gmail.com>
2021-10-19 20:13:39 -0400

    cahngement pour form

--- src/mail.php
+++ src/mail.php
@@ -2,7 +2,7 @@
 $email = $_POST['email'];
 $message = $_POST['message'];
 $formcontent="From: $name \n Message: $message";
-$recipient = "contact@ryanez.ca";
+$recipient = "riyaez@gmail.com";
 $subject = "Portfolio : $email Vous a envoyé un message";
 $mailheader = "From: $email \r\n";
 mail($recipient, $subject, $formcontent, $mailheader) or die("Error!");
