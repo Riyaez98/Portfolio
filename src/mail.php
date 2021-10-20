d93ad3772249127910cb0ad99ce32280bccc605b
Ricardo Yanez <riyaez@gmail.com>
2021-10-19 20:40:27 -0400

    Creation page confirmation courriel

--- src/mail.php
+++ src/mail.php
@@ -2,11 +2,10 @@
 $email = $_POST['email'];
 $message = $_POST['message'];
 $formcontent="From: $name \n Message: $message";
-$recipient = "riyaez@gmail.com";
+$recipient = "contact@ryanez.ca";
 $subject = "Portfolio : $email Vous a envoyé un message";
 $mailheader = "From: $email \r\n";
 mail($recipient, $subject, $formcontent, $mailheader) or die("Error!");
-// echo "Thank You!";
-    // header("Location: contact.html?mailsent");
-    header("Location: 404.html");
+
+    header("Location: confirm-envoie.html");
 ?>
